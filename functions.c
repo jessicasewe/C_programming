21430f71d4544787c42793970668be1e55f163d6
jessicasewe <jessicasewe89@gmail.com>
2023-11-07 15:40:03 +0000

    writing first function

---
+++ functions.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void birthday(){
+    printf("Happy Birthday dumbdumb\n");
+    printf("Happy Birthday dumbdumb\n");
+    printf("Happy Birthday dumbdumb");
+}
+
+int main(){
+    birthday();
+}
