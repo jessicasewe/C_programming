68202ca48077a8b796e8e278588a611bfcde7abc
jessicasewe <jessicasewe89@gmail.com>
2023-11-10 08:40:57 +0000

    draws a line with _ in the terminal

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+void print_line(int n)
+{
+    
+    if(n <= 0){
+        putchar('\n');
+    }
+    else{
+        for(int i = 0; i < n; i++){
+        putchar('-');
+    }
+    }
+    putchar('\n');
+
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_line(0);
+    print_line(2);
+    print_line(10);
+    print_line(-4);
+    return (0);
+}
