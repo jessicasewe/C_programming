ef8c9e3ebe68a559450801e029380230c5ac7738
Jessica Sewe <jessicasewe89@gmail.com>
2023-11-12 08:53:46 +0000

    structs done

---
+++ structs.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+struct Player
+{
+    char name[12];
+    int score;
+};
+
+
+
+int main()
+{
+
+    struct Player player1;
+    struct Player player2;
+
+    strcpy(player1.name, "Jessica"); //use a strcpy when using an array of characters
+    player1.score = 4;
+
+    strcpy(player2.name, "Sewe"); //use a strcpy when using an array of characters
+    player2.score = 5;
+
+    printf("%s\n", player1.name);
+    printf("%d\n", player1.score);
+
+    printf("%s\n", player2.name);
+    printf("%d\n", player2.score);
+
+    return 0;
+
+}
