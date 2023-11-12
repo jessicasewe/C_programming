b76e60b65ed094ff05f6fd5474b590e46dda6fcf
Jessica Sewe <jessicasewe89@gmail.com>
2023-11-12 10:59:57 +0000

    pseudo random numbers

---
+++ random_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * pseudo random numbers
+*/
+
+
+int main()
+{
+
+    srand(time(0));
+
+    int number1 = (rand() % 6) + 1;
+    int number2= (rand() % 6) + 1;
+    int number3 = (rand() % 6) + 1;
+
+    printf("%d\n", number1);
+    printf("%d\n", number2);
+    printf("%d\n", number3);
+
+    return 0;
+}
